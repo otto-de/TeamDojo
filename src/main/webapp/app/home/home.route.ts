503a8a489c03ea7a44bb6f253baf396a70adbb15
lri <lennard.riebandt@iteratec.de>
2018-04-18 17:35:58 +0200

    modified welcome page and replaced logo

--- src/main/webapp/app/home/home.route.ts
+++ src/main/webapp/app/home/home.route.ts
@@ -7,6 +7,6 @@ export const HOME_ROUTE: Route = {
     component: HomeComponent,
     data: {
         authorities: [],
-        pageTitle: 'Welcome, Java Hipster!'
+        pageTitle: 'Otto EC Dojo'
     }
 };
