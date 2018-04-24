99144e71e3f5f2eb118f28b3e195e3eabde02d97
lri <lennard.riebandt@iteratec.de>
2018-04-24 18:19:41 +0200

    EDo-38: Initial commit for skill section

--- src/main/webapp/app/teams/teams.module.ts
+++ src/main/webapp/app/teams/teams.module.ts
@@ -8,12 +8,13 @@ import { TeamsAchievementsComponent } from './teams-achievements.component';
 import { TeamsSkillsComponent } from './teams-skills.component';
 import { TeamsResolve } from './teams.route';
 import { TeamsService } from './teams.service';
+import { TeamsSkillsService } from './teams-skills.service';
 
 @NgModule({
     imports: [TeamdojoSharedModule, RouterModule.forChild([TEAMS_ROUTE])],
     declarations: [TeamsComponent, TeamsStatusComponent, TeamsAchievementsComponent, TeamsSkillsComponent],
     entryComponents: [],
-    providers: [TeamsService, TeamsResolve],
+    providers: [TeamsService, TeamsSkillsService, TeamsResolve],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class TeamsModule {}
