a677b86c940a619937761f98ea7779102b550433
lri <lennard.riebandt@iteratec.at>
2018-04-25 16:14:18 +0200

    EDO-51: added infinite scroll to skill section

--- src/main/java/de/otto/teamdojo/service/impl/AchievableSkillServiceImpl.java
+++ src/main/java/de/otto/teamdojo/service/impl/AchievableSkillServiceImpl.java
@@ -5,6 +5,8 @@ import de.otto.teamdojo.service.AchievableSkillService;
 import de.otto.teamdojo.service.dto.AchievableSkillDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,7 +25,7 @@ public class AchievableSkillServiceImpl implements AchievableSkillService {
     }
 
     @Override
-    public List<AchievableSkillDTO> findAllByTeamId(Long teamId) {
-        return skillRepository.findAchievableSkill(teamId);
+    public Page<AchievableSkillDTO> findAllByTeamId(Long teamId, Pageable pageable) {
+        return skillRepository.findAchievableSkill(teamId, pageable);
     }
 }
