c8aac1cf1f56d3a7fcc9af87a99bab26416f3ed1
Hieu Nguyen <hieu.nguyen@iteratec.de>
2018-05-03 16:52:30 +0200

    Add Team Selection Component

---
+++ src/main/webapp/app/teams/teams-selection/teams-selection.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+// import { TeamsSelectionService } from 'app/teams/teams-selection/teams-selection.service';
+import { TeamsService } from 'app/teams/teams.service';
+import { Team } from 'app/shared/model/team.model';
+
+@Component({
+    selector: 'jhi-teams-selection',
+    templateUrl: './teams-selection.component.html',
+    styleUrls: ['./teams-selection.scss']
+})
+export class TeamsSelectionComponent implements OnInit {
+    private highlightedTeamId = -1;
+
+    private teams: Team[] = [];
+
+    constructor(
+        private activeModal: NgbActiveModal,
+        // private teamsSelectionService: TeamsSelectionService,
+        private teamsService: TeamsService
+    ) {}
+
+    ngOnInit(): void {
+        this.teamsService.query().subscribe(teams => {
+            this.teams = teams.body;
+            console.log(this.teams);
+        });
+    }
+
+    selectTeam(teamId: number) {
+        this.highlightedTeamId = teamId;
+    }
+
+    confirmTeam() {
+        // this.teamsSelectionService.setTeamId(this.highlightedTeamId);
+        this.activeModal.close('Team selected');
+    }
+
+    cancelTeamSelection() {
+        this.activeModal.dismiss('Team selected cancelled');
+    }
+
+    getTeamImage(team: Team) {
+        return team.picture && team.pictureContentType ? `data:${team.pictureContentType};base64,${team.picture}` : null;
+    }
+}
