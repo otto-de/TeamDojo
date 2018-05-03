2b6370cb4635c63b0d9b50333e147ed335307342
lri <lennard.riebandt@iteratec.de>
2018-05-03 17:58:15 +0200

    EDO-42: added toogle switch for skill completion

--- src/main/webapp/app/shared/index.ts
+++ src/main/webapp/app/shared/index.ts
@@ -10,4 +10,5 @@ export * from './util/request-util';
 export * from './shared-libs.module';
 export * from './shared-common.module';
 export * from './shared.module';
+export * from './toogleswitch/toggle-switch.component';
 export * from './util/datepicker-adapter';
