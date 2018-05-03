28bd215ab8e9e38370e84c57d2f5c7b1cf27741c
Steven Berlin <steven-sebastian.berlin@iteratec.de>
2018-05-03 09:47:28 +0200

    Request available badges from backend

--- src/main/webapp/app/teams/teams-achievements.component.ts
+++ src/main/webapp/app/teams/teams-achievements.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { IBadge } from 'app/shared/model/badge.model';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { JhiAlertService } from 'ng-jhipster';
+import { TeamsAchievementsService } from './teams-achievements.service';
 
 @Component({
     selector: 'jhi-teams-achievements',
@@ -6,7 +10,29 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['teams-achievements.scss']
 })
 export class TeamsAchievementsComponent implements OnInit {
-    constructor() {}
+    badges: IBadge[];
+    constructor(private teamsAchievementsService: TeamsAchievementsService, private jhiAlertService: JhiAlertService) {
+        this.badges = [];
+    }
 
-    ngOnInit() {}
+    ngOnInit() {
+        this.loadAll();
+    }
+
+    loadAll() {
+        this.teamsAchievementsService
+            .queryBadges()
+            .subscribe(
+                (res: HttpResponse<IBadge[]>) => res.body.forEach((badge: IBadge) => this.badges.push(badge)),
+                (res: HttpErrorResponse) => this.onError(res.message)
+            );
+    }
+
+    trackId(index: number, item: IBadge) {
+        return item.id;
+    }
+
+    private onError(errorMessage: string) {
+        this.jhiAlertService.error(errorMessage, null, null);
+    }
 }
