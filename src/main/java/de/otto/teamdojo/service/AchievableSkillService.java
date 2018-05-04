425afc8324f1521a70bcee1fa6a44056637dad26
lri <lennard.riebandt@iteratec.de>
2018-05-04 14:17:21 +0200

    EDO-51: added missing import

--- src/main/java/de/otto/teamdojo/service/AchievableSkillService.java
+++ src/main/java/de/otto/teamdojo/service/AchievableSkillService.java
@@ -4,6 +4,8 @@ import de.otto.teamdojo.service.dto.AchievableSkillDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public interface AchievableSkillService {
 
     /**
