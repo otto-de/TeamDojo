bd9f95d0e113d78a81c4cf44f8cef79d4dc65273
Hieu Nguyen <hieu.nguyen@iteratec.de>
2018-05-08 15:04:13 +0200

    Add Teams Overview Subcomponent

--- src/main/webapp/app/overview/overview.module.ts
+++ src/main/webapp/app/overview/overview.module.ts
@@ -4,10 +4,11 @@ import { TeamdojoSharedModule } from 'app/shared';
 import { OVERVIEW_ROUTE } from 'app/overview/overview.route';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { OverviewComponent } from 'app/overview/overview.component';
+import { OverviewTeamsComponent } from 'app/overview/overview-teams/overview-teams.component';
 
 @NgModule({
     imports: [TeamdojoSharedModule, RouterModule.forChild([OVERVIEW_ROUTE]), NgbModule],
-    declarations: [OverviewComponent],
+    declarations: [OverviewComponent, OverviewTeamsComponent],
     entryComponents: [],
     providers: [],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
