66a2b34004089c1571e88de93d3849e1651fd936
Hieu Nguyen <hieu.nguyen@iteratec.de>
2018-05-08 15:04:13 +0200

    Add Teams Overview Subcomponent

---
+++ src/main/webapp/app/overview/overview-teams/overview-teams.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'jhi-overview-teams',
+    templateUrl: './overview-teams.component.html',
+    styleUrls: ['./overview-teams.scss']
+})
+export class OverviewTeamsComponent {}
