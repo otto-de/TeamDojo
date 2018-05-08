0431ee4f1fbaef0b0df4085f362c070fb9a938f0
lri <lennard.riebandt@iteratec.de>
2018-05-08 12:09:58 +0200

    fixed ts-lints

--- src/main/webapp/app/shared/toogleswitch/toggle-switch.component.ts
+++ src/main/webapp/app/shared/toogleswitch/toggle-switch.component.ts
@@ -3,7 +3,7 @@ import { TeamsSelectionService } from 'app/teams/teams-selection/teams-selection
 import { ITeam } from 'app/shared/model/team.model';
 
 @Component({
-    selector: 'toggle-switch',
+    selector: 'jhi-toggle-switch',
     templateUrl: './toggle-switch.component.html',
     styleUrls: ['./toggle-switch.scss']
 })
