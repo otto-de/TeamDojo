a279392a78e654ba8526197f472d3c47bbad4ed5
lri <lennard.riebandt@iteratec.de>
2018-05-11 11:50:45 +0200

    EDO-77: added team list in overview with level and badge count

--- src/main/webapp/app/overview/overview.module.ts
+++ src/main/webapp/app/overview/overview.module.ts
@@ -1,7 +1,15 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TeamdojoSharedModule } from 'app/shared';
-import { OVERVIEW_ROUTE } from 'app/overview/overview.route';
+import {
+    AllBadgeSkillsResolve,
+    AllBadgesResolve,
+    AllLevelSkillsResolve,
+    AllLevelsResolve,
+    AllTeamSkillsResolve,
+    AllTeamsResolve,
+    OVERVIEW_ROUTE
+} from 'app/overview/overview.route';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { OverviewComponent } from 'app/overview/overview.component';
 import { OverviewTeamsComponent } from 'app/overview/teams/overview-teams.component';
@@ -12,7 +20,7 @@ import { OverviewSkillsComponent } from 'app/overview/skills/overview-skills.com
     imports: [TeamdojoSharedModule, RouterModule.forChild([OVERVIEW_ROUTE]), NgbModule],
     declarations: [OverviewComponent, OverviewTeamsComponent, OverviewAchievementsComponent, OverviewSkillsComponent],
     entryComponents: [],
-    providers: [],
+    providers: [AllTeamsResolve, AllLevelsResolve, AllBadgesResolve, AllTeamSkillsResolve, AllLevelSkillsResolve, AllBadgeSkillsResolve],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class OverviewModule {}
--- src/main/webapp/app/overview/overview.route.ts
+++ src/main/webapp/app/overview/overview.route.ts
@@ -1,5 +1,66 @@
-import { Route } from '@angular/router';
+import { ActivatedRouteSnapshot, Resolve, Route, RouterStateSnapshot } from '@angular/router';
 import { OverviewComponent } from './';
+import { Injectable } from '@angular/core';
+import { LevelSkillService } from 'app/entities/level-skill';
+import { TeamService } from 'app/entities/team';
+import { LevelService } from 'app/entities/level';
+import { BadgeService } from 'app/entities/badge';
+import { TeamSkillService } from 'app/entities/team-skill';
+import { BadgeSkillService } from 'app/entities/badge-skill';
+
+@Injectable()
+export class AllTeamsResolve implements Resolve<any> {
+    constructor(private teamService: TeamService) {}
+
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        return this.teamService.query();
+    }
+}
+
+@Injectable()
+export class AllLevelsResolve implements Resolve<any> {
+    constructor(private levelService: LevelService) {}
+
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        return this.levelService.query();
+    }
+}
+
+@Injectable()
+export class AllBadgesResolve implements Resolve<any> {
+    constructor(private badgeService: BadgeService) {}
+
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        return this.badgeService.query();
+    }
+}
+
+@Injectable()
+export class AllTeamSkillsResolve implements Resolve<any> {
+    constructor(private teamSkillService: TeamSkillService) {}
+
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        return this.teamSkillService.query();
+    }
+}
+
+@Injectable()
+export class AllLevelSkillsResolve implements Resolve<any> {
+    constructor(private levelSkillService: LevelSkillService) {}
+
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        return this.levelSkillService.query();
+    }
+}
+
+@Injectable()
+export class AllBadgeSkillsResolve implements Resolve<any> {
+    constructor(private badgeSkillService: BadgeSkillService) {}
+
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        return this.badgeSkillService.query();
+    }
+}
 
 export const OVERVIEW_ROUTE: Route = {
     path: '',
@@ -7,5 +68,13 @@ export const OVERVIEW_ROUTE: Route = {
     data: {
         authorities: [],
         pageTitle: 'teamdojoApp.teams.home.title'
+    },
+    resolve: {
+        teams: AllTeamsResolve,
+        levels: AllLevelsResolve,
+        badges: AllBadgesResolve,
+        teamSkills: AllTeamSkillsResolve,
+        levelSkills: AllLevelSkillsResolve,
+        badgeSkills: AllBadgeSkillsResolve
     }
 };
