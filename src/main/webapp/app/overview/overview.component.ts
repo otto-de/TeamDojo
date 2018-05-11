46747fce7aaea98b448c35727c4e0fe64c597cc8
lri <lennard.riebandt@iteratec.de>
2018-05-11 11:50:45 +0200

    EDO-77: added team list in overview with level and badge count

--- src/main/webapp/app/overview/overview.component.ts
+++ src/main/webapp/app/overview/overview.component.ts
@@ -1,8 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ITeam } from 'app/shared/model/team.model';
+import { ILevel } from 'app/shared/model/level.model';
+import { IBadge } from 'app/shared/model/badge.model';
+import { ITeamSkill } from 'app/shared/model/team-skill.model';
+import { ILevelSkill } from 'app/shared/model/level-skill.model';
+import { sortLevels } from 'app/shared';
+import { IBadgeSkill } from 'app/shared/model/badge-skill.model';
 
 @Component({
     selector: 'jhi-overview',
     templateUrl: './overview.component.html',
     styleUrls: ['./overview.scss']
 })
-export class OverviewComponent {}
+export class OverviewComponent implements OnInit {
+    teams: ITeam[];
+    levels: ILevel[];
+    badges: IBadge[];
+    teamSkills: ITeamSkill[];
+    levelSkills: ILevelSkill[];
+    badgeSkills: IBadgeSkill[];
+
+    constructor(private route: ActivatedRoute) {}
+
+    ngOnInit() {
+        this.route.data.subscribe(({ teams, levels, badges, teamSkills, levelSkills, badgeSkills }) => {
+            this.teams = teams.body;
+            this.levels = levels.body;
+            this.badges = badges.body;
+            this.teamSkills = teamSkills.body;
+            this.levelSkills = levelSkills.body;
+            this.badgeSkills = badgeSkills.body;
+
+            const groupedTeamSkills = {};
+            this.teamSkills.forEach(teamSkill => {
+                groupedTeamSkills[teamSkill.teamId] = groupedTeamSkills[teamSkill.teamId] || [];
+                groupedTeamSkills[teamSkill.teamId].push(Object.assign(teamSkill));
+            });
+
+            const groupedLevelSkills = {};
+            this.levelSkills.forEach(levelSkill => {
+                groupedLevelSkills[levelSkill.levelId] = groupedLevelSkills[levelSkill.levelId] || [];
+                groupedLevelSkills[levelSkill.levelId].push(Object.assign(levelSkill));
+            });
+
+            const groupedLevels = {};
+            this.levels.forEach(level => {
+                groupedLevels[level.dimensionId] = groupedLevels[level.dimensionId] || [];
+                groupedLevels[level.dimensionId].push(Object.assign(level, { skills: groupedLevelSkills[level.id] }));
+            });
+            for (const dimensionId in groupedLevels) {
+                if (groupedLevels.hasOwnProperty(dimensionId)) {
+                    groupedLevels[dimensionId] = sortLevels(groupedLevels[dimensionId]).reverse();
+                }
+            }
+
+            const groupedBadgeSkills = {};
+            this.badgeSkills.forEach(badgeSkill => {
+                groupedBadgeSkills[badgeSkills.badgeId] = groupedBadgeSkills[badgeSkill.badgeId] || [];
+                groupedBadgeSkills[badgeSkills.badgeId].push(Object.assign(badgeSkill));
+            });
+
+            this.badges.forEach(badge => {
+                badge.skills = groupedBadgeSkills[badge.id] || [];
+            });
+
+            this.teams.forEach(team => {
+                team.skills = groupedTeamSkills[team.id] || [];
+                team.participations.forEach(dimension => {
+                    dimension.levels = groupedLevels[dimension.id] || [];
+                });
+            });
+        });
+    }
+}
