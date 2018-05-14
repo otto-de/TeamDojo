868ca129bfd49bf784b77c84af9451e3963fc6ed
Hieu Nguyen <hieu.nguyen@iteratec.de>
2018-05-14 21:15:59 +0200

    EDO-37: display informations on detail page

--- src/main/webapp/app/teams/skill-details/skill-details.component.ts
+++ src/main/webapp/app/teams/skill-details/skill-details.component.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ITeam } from 'app/shared/model/team.model';
 import { ISkill } from 'app/shared/model/skill.model';
+import { TeamSkillService } from 'app/entities/team-skill';
+import { TeamsService } from 'app/teams/teams.service';
+import { IBadge } from 'app/shared/model/badge.model';
+import { ILevel } from 'app/shared/model/level.model';
+import { LevelService } from 'app/entities/level';
+import { BadgeService } from 'app/entities/badge';
 
 @Component({
     selector: 'jhi-skill-details',
@@ -13,16 +19,65 @@ export class SkillDetailsComponent implements OnInit {
 
     private skill: ISkill;
 
-    constructor(private route: ActivatedRoute) {}
+    private achievedByTeams: ITeam[] = [];
+
+    private neededForLevels: ILevel[] = [];
+
+    private neededForBadges: IBadge[] = [];
+
+    constructor(
+        private route: ActivatedRoute,
+        private teamSkillService: TeamSkillService,
+        private teamsService: TeamsService,
+        private levelService: LevelService,
+        private badgeService: BadgeService
+    ) {}
 
     ngOnInit(): void {
         this.route.data.subscribe(({ team, skill }) => {
             this.team = team.body[0] ? team.body[0] : team;
             this.skill = skill;
         });
+        this.loadData();
+    }
+
+    loadData() {
+        this.achievedByTeams = [];
+        this.neededForLevels = [];
+        this.neededForBadges = [];
+
+        this.teamSkillService.query({ 'skillId.equals': this.skill.id, 'completedAt.specified': true }).subscribe(res => {
+            const teamsId = res.body.map(ts => ts.teamId);
+            if (teamsId.length !== 0) {
+                this.teamsService.query({ 'id.in': teamsId }).subscribe(teamsRes => {
+                    this.achievedByTeams = teamsRes.body;
+                });
+            }
+        });
+
+        this.levelService.query({ 'skillId.in': this.skill.id }).subscribe(res => {
+            this.neededForLevels = res.body;
+        });
+
+        this.badgeService.query({ 'skillId.in': this.skill.id }).subscribe(res => {
+            this.neededForBadges = res.body;
+        });
     }
 
     handleSkillChange(skill: ISkill) {
         this.skill = skill;
+        this.loadData();
+    }
+
+    getTeamImage(team: ITeam) {
+        return team.picture && team.pictureContentType ? `data:${team.pictureContentType};base64,${team.picture}` : '';
+    }
+
+    getLevelImage(level: ILevel) {
+        return level.picture && level.pictureContentType ? `data:${level.pictureContentType};base64,${level.picture}` : '';
+    }
+
+    getBadgeImage(badge: IBadge) {
+        return badge.logo && badge.logoContentType ? `data:${badge.logoContentType};base64,${badge.logo}` : '';
     }
 }
