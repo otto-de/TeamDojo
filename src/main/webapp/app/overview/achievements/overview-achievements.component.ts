08cfe178c6b21c62973b5199ee891bb8af77b65f
lri <lennard.riebandt@iteratec.de>
2018-05-14 14:15:58 +0200

    EDO-81: added achievements section to overview

--- src/main/webapp/app/overview/achievements/overview-achievements.component.ts
+++ src/main/webapp/app/overview/achievements/overview-achievements.component.ts
@@ -1,8 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ILevel } from 'app/shared/model/level.model';
+import { DimensionService } from 'app/entities/dimension';
+import { IDimension } from 'app/shared/model/dimension.model';
+import { HttpResponse } from '@angular/common/http';
+import { IBadge } from 'app/shared/model/badge.model';
 
 @Component({
     selector: 'jhi-overview-achievements',
     templateUrl: './overview-achievements.component.html',
     styleUrls: ['./overview-achievements.scss']
 })
-export class OverviewAchievementsComponent {}
+export class OverviewAchievementsComponent implements OnInit {
+    @Input() levels: ILevel[];
+    @Input() badges: IBadge[];
+    dimensions: IDimension[];
+    generalBadges: IBadge[];
+
+    constructor(private dimensionService: DimensionService) {}
+
+    ngOnInit(): void {
+        this.generalBadges = [];
+        this.dimensionService.query().subscribe((res: HttpResponse<IDimension[]>) => {
+            this.dimensions = res.body;
+            const levelsByDimension = {};
+            this.levels.forEach((level: ILevel) => {
+                levelsByDimension[level.dimensionId] = levelsByDimension[level.dimensionId] || [];
+                levelsByDimension[level.dimensionId].push(Object.assign(level));
+            });
+
+            const badgesByDimensionId = {};
+            this.badges.forEach((badge: IBadge) => {
+                if (badge.dimensions && badge.dimensions.length) {
+                    badge.dimensions.forEach((dimension: IDimension) => {
+                        badgesByDimensionId[dimension.id] = badgesByDimensionId[dimension.id] || [];
+                        badgesByDimensionId[dimension.id].push(Object.assign(badge));
+                    });
+                } else {
+                    this.generalBadges.push(Object.assign(badge));
+                }
+            });
+
+            this.dimensions.forEach((dimension: IDimension) => {
+                dimension.levels = levelsByDimension[dimension.id] || [];
+                dimension.badges = badgesByDimensionId[dimension.id] || [];
+            });
+            console.log(this.dimensions);
+        });
+    }
+}
