d43959bbd2f96f37b57e4abf0f3c1711bf7a95e7
Steven Berlin <steven-sebastian.berlin@iteratec.de>
2018-05-17 14:36:05 +0200

    Revert "Use DOM sanitizer to allow empty resource URI"
    This reverts commit e9ec4e46c1dc5cc62c86d8d4cbb2dae2e0f3f13c.

--- src/main/webapp/app/shared/pipe/image-data-url.pipe.ts
+++ src/main/webapp/app/shared/pipe/image-data-url.pipe.ts
@@ -1,11 +1,8 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
 
 @Pipe({ name: 'imageDataUrl' })
 export class ImageDataUrlPipe implements PipeTransform {
-    constructor(private sanitizer: DomSanitizer) {}
-
-    transform(imageData: string, mediaType: string): any {
-        return imageData && mediaType ? `data:${mediaType};base64,${imageData}` : this.sanitizer.bypassSecurityTrustResourceUrl('');
+    transform(imageData: string, mediaType: string): string {
+        return imageData && mediaType ? `data:${mediaType};base64,${imageData}` : '';
     }
 }
