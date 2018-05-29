d1319be1e16d56018ac6334c8dd7ac9414630b9a
Steven Berlin <steven-sebastian.berlin@iteratec.de>
2018-05-29 09:25:28 +0200

    EDO-24 Replace mocked comments with real data

--- src/main/webapp/app/teams/skill-details/skill-details-comments/skill-details-comments.component.ts
+++ src/main/webapp/app/teams/skill-details/skill-details-comments/skill-details-comments.component.ts
@@ -1,8 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ITeam } from 'app/shared/model/team.model';
+import { TeamsSelectionService } from 'app/teams/teams-selection/teams-selection.service';
+import { Comment, IComment } from 'app/shared/model/comment.model';
+import { ISkill } from 'app/shared/model/skill.model';
+import { ActivatedRoute } from '@angular/router';
+import { CommentService } from 'app/entities/comment';
+import { HttpResponse } from '@angular/common/http';
+import * as moment from 'moment';
 
 @Component({
     selector: 'jhi-skill-details-comments',
     templateUrl: './skill-details-comments.component.html',
     styleUrls: ['./skill-details-comments.scss']
 })
-export class SkillDetailsCommentsComponent {}
+export class SkillDetailsCommentsComponent implements OnInit {
+    team: ITeam;
+    skill: ISkill;
+    newComment: IComment;
+
+    constructor(
+        private commentService: CommentService,
+        private teamsSelectionService: TeamsSelectionService,
+        private route: ActivatedRoute
+    ) {}
+
+    ngOnInit() {
+        this.newComment = new Comment();
+        this.route.data.subscribe(({ team, skill }) => {
+            this.skill = skill.body ? skill.body : skill;
+            this.team = team.body ? team.body : team;
+        });
+    }
+
+    isActiveTeam() {
+        return this.teamsSelectionService.selectedTeam && this.team && this.teamsSelectionService.selectedTeam.id === this.team.id;
+    }
+
+    onSubmit() {
+        this.newComment.creationDate = moment();
+        this.newComment.skillId = this.skill ? this.skill.id : undefined;
+        this.newComment.skillTitle = this.skill ? this.skill.title : undefined;
+        this.newComment.teamId = this.team ? this.team.id : undefined;
+        this.newComment.teamShortName = this.team ? this.team.shortName : undefined;
+        this.commentService.create(this.newComment).subscribe((res: HttpResponse<IComment>) => {
+            if (res.body) {
+                this.skill.comments.push(res.body);
+                this.newComment = new Comment();
+            }
+        });
+    }
+}
