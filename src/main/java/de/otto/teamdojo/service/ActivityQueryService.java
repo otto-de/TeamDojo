dba2475baa768bd93e6e82153fe9f2f4d86148e0
Hieu Nguyen <hieu.nguyen@iteratec.de>
2018-05-29 14:57:38 +0200

    EDO-52: generate Activity entity with jhipster

---
+++ src/main/java/de/otto/teamdojo/service/ActivityQueryService.java
@@ -0,0 +1,92 @@
+package de.otto.teamdojo.service;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import io.github.jhipster.service.QueryService;
+
+import de.otto.teamdojo.domain.Activity;
+import de.otto.teamdojo.domain.*; // for static metamodels
+import de.otto.teamdojo.repository.ActivityRepository;
+import de.otto.teamdojo.service.dto.ActivityCriteria;
+
+import de.otto.teamdojo.service.dto.ActivityDTO;
+import de.otto.teamdojo.service.mapper.ActivityMapper;
+
+/**
+ * Service for executing complex queries for Activity entities in the database.
+ * The main input is a {@link ActivityCriteria} which gets converted to {@link Specification},
+ * in a way that all the filters must apply.
+ * It returns a {@link List} of {@link ActivityDTO} or a {@link Page} of {@link ActivityDTO} which fulfills the criteria.
+ */
+@Service
+@Transactional(readOnly = true)
+public class ActivityQueryService extends QueryService<Activity> {
+
+    private final Logger log = LoggerFactory.getLogger(ActivityQueryService.class);
+
+    private final ActivityRepository activityRepository;
+
+    private final ActivityMapper activityMapper;
+
+    public ActivityQueryService(ActivityRepository activityRepository, ActivityMapper activityMapper) {
+        this.activityRepository = activityRepository;
+        this.activityMapper = activityMapper;
+    }
+
+    /**
+     * Return a {@link List} of {@link ActivityDTO} which matches the criteria from the database
+     * @param criteria The object which holds all the filters, which the entities should match.
+     * @return the matching entities.
+     */
+    @Transactional(readOnly = true)
+    public List<ActivityDTO> findByCriteria(ActivityCriteria criteria) {
+        log.debug("find by criteria : {}", criteria);
+        final Specification<Activity> specification = createSpecification(criteria);
+        return activityMapper.toDto(activityRepository.findAll(specification));
+    }
+
+    /**
+     * Return a {@link Page} of {@link ActivityDTO} which matches the criteria from the database
+     * @param criteria The object which holds all the filters, which the entities should match.
+     * @param page The page, which should be returned.
+     * @return the matching entities.
+     */
+    @Transactional(readOnly = true)
+    public Page<ActivityDTO> findByCriteria(ActivityCriteria criteria, Pageable page) {
+        log.debug("find by criteria : {}, page: {}", criteria, page);
+        final Specification<Activity> specification = createSpecification(criteria);
+        return activityRepository.findAll(specification, page)
+            .map(activityMapper::toDto);
+    }
+
+    /**
+     * Function to convert ActivityCriteria to a {@link Specification}
+     */
+    private Specification<Activity> createSpecification(ActivityCriteria criteria) {
+        Specification<Activity> specification = Specification.where(null);
+        if (criteria != null) {
+            if (criteria.getId() != null) {
+                specification = specification.and(buildSpecification(criteria.getId(), Activity_.id));
+            }
+            if (criteria.getType() != null) {
+                specification = specification.and(buildSpecification(criteria.getType(), Activity_.type));
+            }
+            if (criteria.getData() != null) {
+                specification = specification.and(buildStringSpecification(criteria.getData(), Activity_.data));
+            }
+            if (criteria.getCreatedAt() != null) {
+                specification = specification.and(buildRangeSpecification(criteria.getCreatedAt(), Activity_.createdAt));
+            }
+        }
+        return specification;
+    }
+
+}
---
+++ src/main/java/de/otto/teamdojo/service/dto/ActivityDTO.java
@@ -0,0 +1,86 @@
+package de.otto.teamdojo.service.dto;
+
+import java.time.Instant;
+import javax.validation.constraints.*;
+import java.io.Serializable;
+import java.util.Objects;
+import de.otto.teamdojo.domain.enumeration.ActivityType;
+
+/**
+ * A DTO for the Activity entity.
+ */
+public class ActivityDTO implements Serializable {
+
+    private Long id;
+
+    @NotNull
+    private ActivityType type;
+
+    private String data;
+
+    @NotNull
+    private Instant createdAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public ActivityType getType() {
+        return type;
+    }
+
+    public void setType(ActivityType type) {
+        this.type = type;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        ActivityDTO activityDTO = (ActivityDTO) o;
+        if (activityDTO.getId() == null || getId() == null) {
+            return false;
+        }
+        return Objects.equals(getId(), activityDTO.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "ActivityDTO{" +
+            "id=" + getId() +
+            ", type='" + getType() + "'" +
+            ", data='" + getData() + "'" +
+            ", createdAt='" + getCreatedAt() + "'" +
+            "}";
+    }
+}
