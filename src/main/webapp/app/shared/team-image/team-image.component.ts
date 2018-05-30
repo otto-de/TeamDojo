d1d0f892b106edb97fb82ed0a32ae65f878e9789
Steven Berlin <steven-sebastian.berlin@iteratec.de>
2018-05-30 17:39:52 +0200

    EDO-121 Move team image from team status to separate component

---
+++ src/main/webapp/app/shared/team-image/team-image.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { ITeam } from 'app/shared/model/team.model';
+
+@Component({
+    selector: 'jhi-team-image',
+    templateUrl: './team-image.component.html',
+    styleUrls: ['./team-image.scss']
+})
+export class TeamImageComponent {
+    @Input() team: ITeam;
+    constructor() {}
+}
