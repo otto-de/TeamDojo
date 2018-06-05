876cc349389731cfeb9ea4ed0ccfcecf79e40767
Hieu Nguyen <hieu.nguyen@iteratec.de>
2018-06-05 17:29:30 +0200

    EDO-74: scale uploaded image

--- src/main/java/de/otto/teamdojo/service/impl/ImageServiceImpl.java
+++ src/main/java/de/otto/teamdojo/service/impl/ImageServiceImpl.java
@@ -11,6 +11,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.Optional;
 import java.util.LinkedList;
 import java.util.List;
@@ -42,9 +48,56 @@ public class ImageServiceImpl implements ImageService {
     @Override
     public ImageDTO save(ImageDTO imageDTO) {
         log.debug("Request to save Image : {}", imageDTO);
+
+        BufferedImage img = createImageFromBytes(imageDTO.getLarge());
+        BufferedImage large = resize(img, 1.0);
+        BufferedImage medium = resize(img,  0.5);
+        BufferedImage small = resize(img, 0.25);
+        imageDTO.setLarge(getByteArrayFromBufferedImage(large));
+        imageDTO.setLargeContentType("image/png");
+        imageDTO.setMedium(getByteArrayFromBufferedImage(medium));
+        imageDTO.setMediumContentType("image/png");
+        imageDTO.setSmall(getByteArrayFromBufferedImage(small));
+        imageDTO.setSmallContentType("image/png");
+
         Image image = imageMapper.toEntity(imageDTO);
         image = imageRepository.save(image);
         return imageMapper.toDto(image);
+    }
+
+    private byte[] getByteArrayFromBufferedImage(BufferedImage img) {
+        try {
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write(img, "png", baos);
+            baos.flush();
+            byte[] imageInByte = baos.toByteArray();
+            baos.close();
+            return imageInByte;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    private BufferedImage resize(BufferedImage img, double ratio) {
+        int width = (int) (img.getWidth() * ratio);
+        int height = (int) (img.getHeight() * ratio);
+        java.awt.Image tmp = img.getScaledInstance(width, height, java.awt.Image.SCALE_SMOOTH);
+        BufferedImage resized = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = resized.createGraphics();
+        g2d.drawImage(tmp, 0, 0, null);
+        g2d.dispose();
+        return resized;
+    }
+
+    private BufferedImage createImageFromBytes(byte[] imageData) {
+        ByteArrayInputStream bais = new ByteArrayInputStream(imageData);
+        try {
+            return ImageIO.read(bais);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
