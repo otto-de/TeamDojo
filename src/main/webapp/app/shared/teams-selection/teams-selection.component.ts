059f2413df21c49ff7c47a87a7d78393ba8ca01c
Steven Berlin <steven-sebastian.berlin@iteratec.de>
2018-06-06 11:45:20 +0200

    EDO-121 Use team image component for team selection images

--- src/main/webapp/app/shared/teams-selection/teams-selection.component.ts
+++ src/main/webapp/app/shared/teams-selection/teams-selection.component.ts
@@ -51,8 +51,4 @@ export class TeamsSelectionComponent implements OnInit {
     cancelTeamSelection() {
         this.activeModal.dismiss('Team selected cancelled');
     }
-
-    getTeamImage(team: ITeam) {
-        return team.picture && team.pictureContentType ? `data:${team.pictureContentType};base64,${team.picture}` : null;
-    }
 }
