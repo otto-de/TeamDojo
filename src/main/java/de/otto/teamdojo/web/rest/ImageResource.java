524e03d69b9ab2166afc94474c91845119826c56
Hieu Nguyen <hieu.nguyen@iteratec.de>
2018-06-06 17:57:40 +0200

    EDO-138: caching for Image

--- src/main/java/de/otto/teamdojo/web/rest/ImageResource.java
+++ src/main/java/de/otto/teamdojo/web/rest/ImageResource.java
@@ -10,10 +10,7 @@ import de.otto.teamdojo.service.ImageQueryService;
 import io.github.jhipster.web.util.ResponseUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
@@ -21,6 +18,7 @@ import java.net.URISyntaxException;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 /**
  * REST controller for managing Image.
@@ -143,6 +141,7 @@ public class ImageResource {
         }
         return ResponseEntity.ok()
             .header("Content-Type", contentType)
+            .cacheControl(CacheControl.maxAge(30, TimeUnit.DAYS))
             .body(imageBlob);
     }
 
