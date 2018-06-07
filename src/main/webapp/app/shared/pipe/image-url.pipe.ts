290141bab14e96e95c35070188aac9580346cf4f
Steven Berlin <steven-sebastian.berlin@iteratec.de>
2018-06-07 15:53:05 +0200

    EDO-74 Accept different types of team id as argument in pipe
    Required to build production

--- src/main/webapp/app/shared/pipe/image-url.pipe.ts
+++ src/main/webapp/app/shared/pipe/image-url.pipe.ts
@@ -2,7 +2,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'imageUrl' })
 export class ImageUrlPipe implements PipeTransform {
-    transform(imageId: string, size: string): string {
+    transform(imageId: string | number, size: string): string {
         return imageId ? `/api/images/${imageId}/content?size=${size}` : '';
     }
 }
