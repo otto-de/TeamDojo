d1c5796a6cfc2ba80701d09d80946a089b2e15bf
Rafael Kaschuba <rafael.kaschuba@iteratec.de>
2018-06-07 17:37:26 +0200

    Fix error for skill.validation and skill.implementation

--- src/main/webapp/app/shared/pipe/skill-filter.pipe.ts
+++ src/main/webapp/app/shared/pipe/skill-filter.pipe.ts
@@ -7,12 +7,7 @@ export class SkillFilterPipe implements PipeTransform {
     transform(skills: ISkill[], searchString: string): ISkill[] {
         return searchString.length > 2
             ? skills.filter(skill => {
-                  return (
-                      skill.title.includes(searchString) ||
-                      skill.description.includes(searchString) ||
-                      skill.implementation.includes(searchString) ||
-                      skill.validation.includes(searchString)
-                  );
+                  return skill.title.includes(searchString) || skill.description.includes(searchString);
               })
             : skills;
     }
