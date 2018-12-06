e99ba6ca9d68488dbf377284bfc71016487a4116
Steven Berlin <steven-sebastian.berlin@iteratec.de>
2018-12-06 12:44:55 +0100

    Raise skill attribute character limits to match new initial data

--- src/main/java/de/otto/teamdojo/service/dto/SkillDTO.java
+++ src/main/java/de/otto/teamdojo/service/dto/SkillDTO.java
@@ -15,10 +15,13 @@ public class SkillDTO implements Serializable {
     @Size(min = 5, max = 80)
     private String title;
 
+    @Size(max = 2048)
     private String description;
 
+    @Size(max = 2048)
     private String implementation;
 
+    @Size(max = 2048)
     private String validation;
 
     @Pattern(regexp = "^P(?:([-+]?[0-9]+)Y)?(?:([-+]?[0-9]+)M)?(?:([-+]?[0-9]+)W)?(?:([-+]?[0-9]+)D)?$")
