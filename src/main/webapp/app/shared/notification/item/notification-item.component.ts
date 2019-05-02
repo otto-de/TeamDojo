48458ab9c520c039205ac6f01378e68efe11b8a8
Nils Hasenbanck <nils.hasenbanck@trendig.com>
2019-05-02 13:57:22 +0200

    Fix notification displaying events of deleted ressources
    Since notifications of deleted badges / teams are not deleted, we
    need to handle this edge case.
    
    Author: NiHa, LuGe

--- src/main/webapp/app/shared/notification/item/notification-item.component.ts
+++ src/main/webapp/app/shared/notification/item/notification-item.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnChanges } from '@angular/core';
 import { INotification } from 'app/shared/notification/model/notification.model';
-import { IBadge } from 'app/shared/model/badge.model';
-import { ITeam } from 'app/shared/model/team.model';
+import { IBadge, Badge } from 'app/shared/model/badge.model';
+import { ITeam, Team } from 'app/shared/model/team.model';
 
 @Component({
     selector: 'jhi-notification-item',
@@ -22,10 +22,16 @@ export class NotificationItemComponent implements OnChanges {
         const type = this.notification.activity.type;
         if (type.toString() === 'BADGE_CREATED') {
             const badgeId = this.notification.data.badgeId;
-            this.item = this.badges.find((b: IBadge) => b.id === badgeId);
+
+            // Badge could be deleted, in that case use the default.
+            const badge = this.badges.find((b: IBadge) => b.id === badgeId);
+            this.item = badge ? badge : new Badge(0);
         } else if (type.toString() === 'SKILL_COMPLETED') {
             const teamId = this.notification.data.teamId;
-            this.item = this.teams.find((t: ITeam) => t.id === teamId);
+
+            // Team could be deleted, in that case use the default.
+            const team = this.teams.find((t: ITeam) => t.id === teamId);
+            this.item = team ? team : new Team(0);
         }
     }
 
