c2e2ba68eeafa9252f8661e6dbf6a8ded6295c03
Nils Hasenbanck <nils.hasenbanck@otto.de>
2020-09-29 07:04:05 +0200

    Update jib process.

--- src/main/jib/entrypoint.sh
+++ src/main/jib/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "The application will start in ${JHIPSTER_SLEEP}s..." && sleep ${JHIPSTER_SLEEP}
-exec java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "de.otto.teamdojo.TeamdojoApp"  "$@"
+exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "de.otto.teamdojo.TeamdojoApp"  "$@"
